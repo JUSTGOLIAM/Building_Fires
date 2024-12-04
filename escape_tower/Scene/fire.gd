bfd7872ea9716fd1f9cd51ead5f7dc857d3d054a
LEADGREY <august60@126.com>
2024-12-04 12:39:34 -0500

    火圈+opening menu

--- escape_tower/Scene/fire.gd
+++ escape_tower/Scene/fire.gd
@@ -1,12 +1,12 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 func firegwrowth():
-	scale.x-=0.0005
-	scale.y-=0.0005
+	scale.x-=0.005
+	scale.y-=0.005
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,5 +18,5 @@ func _on_player_playermoved() -> void:
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	print (body)
+	print ("die die die")
 	
