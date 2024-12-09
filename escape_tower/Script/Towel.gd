f958b4e325c4af515de4e56710465533128b0c9a
Liam Deng <152089177+JUSTGOLIAM@users.noreply.github.com>
2024-12-09 10:04:22 -0500

    1

--- escape_tower/Script/Towel.gd
+++ escape_tower/Script/Towel.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
-
+@export var dialogue: Array[String]
 
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		$DialogueBox.show()
+		playernearby = ture
