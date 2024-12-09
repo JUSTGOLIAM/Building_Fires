960b740bb5cd8dd5b96b5c19e4b5196c6e86f582
LEADGREY <august60@126.com>
2024-12-09 18:17:03 -0500

    button风格

---
+++ escape_tower/Scene/fire_level1.gd
@@ -0,0 +1,24 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+func firegwrowth():
+	scale.x-=0.0035
+	scale.y-=0.0035
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_playermoved() -> void:
+	firegwrowth()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		print ("die die die")
+		get_tree().change_scene_to_file("res://Scene/next/dead_tutorial.tscn")
+	
