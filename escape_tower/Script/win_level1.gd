305803344847c7c7e56130aa3f08f9b61e50b56a
LEADGREY <august60@126.com>
2024-12-15 18:49:29 -0500

    调整ending

--- escape_tower/Script/win_level1.gd
+++ escape_tower/Script/win_level1.gd
@@ -2,7 +2,8 @@ extends Area2D
 @export var nextScene: PackedScene
 
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		print ("next")
-		get_tree().change_scene_to_packed(nextScene)
+		get_tree().change_scene_to_file("res://Scene/home.tscn")
